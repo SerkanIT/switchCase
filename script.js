946309b1106a85b65aa5ca319d0a423a59077656
SerkanIT <157528566+SerkanIT@users.noreply.github.com>
2024-02-22 19:34:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// ternarny operator //
+// switch case //
+// if else //
+
+// let a = 10;
+
+// a < 20 ? console.log(true) : console.log(false);
+let question1 = +prompt(
+  "Write the Apples CEO \n 1) Steve Jobs \n 2) Paul Walker \n 3) Mark zutenberg \n 4) Dominic Toretto"
+);
+
+switch (question1) {
+  case 1:
+    alert("Bravo, Correct");
+    break;
+  default:
+    alert("Oh No!, Your answer is incorrect");
+}
+
+let question2 = prompt(
+  "Какой Язык Програмирование Даст Нам Пособность Сделать Сайт БЕЗ СТИЛА? \n 1) Python \n 2) Vue \n 3) JavaScript \n 4) HTML"
+);
+
+switch (question2) {
+  case "4":
+    alert("Bravo, Correct");
+    break;
+  default:
+    alert("Oh No! Your answer is incorrect");
+}
+
+let question3 = prompt(
+  "Какой Язык Програмирование Даст Нам Пособность Сделать Сайт СО СТИЛЬОМ? \n 1) Python \n 2) Cascading Style Sheets \n 3) JavaScript \n 4) HyperTextMarkupLanguage"
+);
+
+switch (question3) {
+  case "2":
+    alert("Bravo, Correct");
+    break;
+  default:
+    alert("Oh No! Your answer is incorrect");
+}
+
+let question4 = prompt(
+  "Какой Язык Програмирование Даст Нам Пособность Сделать Сайт ЖЫВИМ? \n 1) Java Script  \n 2) Cascading Style Sheets \n 3) Python \n 4) HyperTextMarkupLanguage"
+);
+
+switch (question2) {
+  case "1":
+    alert("Bravo, Correct");
+    break;
+  default:
+    alert("Oh No! Your answer is incorrect");
+}
